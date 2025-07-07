5de565b54645eb82f3dfd5fa7e85d13ebf8a2564
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-08 00:27:38 +0530

    code

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ app.post("/signin", (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000/signup");
+    console.log("Server is running on http://localhost:3000");
 });
