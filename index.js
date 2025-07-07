3a6c50ebca5e4aaa7bef9cbd59c6630e89a0696d
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-07 19:43:46 +0530

    code

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const users = [];
 app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    users.push({
+        username: username,
+        password: password
+
+    })
     res.json({
-        message: "User signed up successfully"})
+        message:"you have signed in",
+    })
+
+    
 
 })
 app.post("/signin",function(req, res){
