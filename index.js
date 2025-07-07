f478ebc78ffa50340346b08de41e9fb2b68c8685
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-07 18:48:45 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.post("/signup", function(req, res){
+    res.json({
+        message: "User signed up successfully"})
 
 })
 app.post("/signin",function(req, res){
