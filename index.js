8d50b2ba870a9db0dc1298f7488b72d320f00082
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-07 23:51:27 +0530

    code

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
 const express = require("express");
 const app = express();
-const users = [];
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+    const options = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let token = "";
     for (let i = 0; i < 32; i++) {
-        // use a simple function here
         token += options[Math.floor(Math.random() * options.length)];
     }
     return token;
 }
-    
 
-app.post("/signup", function(req, res){
+app.post("/signup", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    if (username.length < 5){
-        res.json({
-            message: "hi there",
-        })
-        return;
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
     }
 
-})    
+    if (username.length < 5) {
+        return res.json({
+            message: "Your username is very short"
+        });
+    }
+
+    // Check if user already exists
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(409).json({
+            message: "Username already exists"
+        });
+    }
+
     users.push({
         username: username,
         password: password
+    });
 
-    })
     res.json({
-        message:"you have signed in",
-    })
-
-    
+        message: "You have signed up successfully"
+    });
+});
 
 app.post("/signin", (req, res) => {
     const username = req.body.username;
@@ -45,13 +56,19 @@ app.post("/signin", (req, res) => {
     if (user) {
         const token = generateToken();
         user.token = token;
-        res.send({
-            token
-        })
-        console.log(users);
+        res.json({
+            message: "Login successful",
+            token: token
+        });
+        console.log(users); // for debugging
     } else {
-        res.status(403).send({
+        res.status(403).json({
             message: "Invalid username or password"
-        })
+        });
     }
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
 });
