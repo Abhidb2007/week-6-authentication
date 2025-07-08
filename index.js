5b18b1edc24f86d979a2821df27fd6003ce200ac
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-08 12:03:53 +0530

    code

--- index.js
+++ index.js
@@ -93,6 +93,6 @@ app.get("/me", (req, res) => {
     }
 });
 // Start the server
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log("Server is running on http://localhost:3000");
 });
